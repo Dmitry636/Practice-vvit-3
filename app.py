91b31055d2504ef94f634f883099ee291e85ea6c
Dmitry636 <dexus636@gmail.com>
2021-11-28 23:08:08 +0300

    flask-bd-linux

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from flask import Flask, render_template, request, redirect
 
 import psycopg2
 
+# создание приложение
 app = Flask(__name__)
 
+# подключение к базе данных и добавление курсора для обращения к ней
 conn = psycopg2.connect(database="service_db",
                         user="postgres",
                         password="dexus636",
@@ -14,12 +16,15 @@ conn = psycopg2.connect(database="service_db",
 
 cursor = conn.cursor()
 
+# создание декоратора, который сравнивает данные введеные пользователем с базой данных
 @app.route('/login/', methods=['POST', 'GET'])
-def login():
+def login():  
     if request.method == 'POST':
         if request.form.get("login"):
+            # забираем значения введенные пользователем username и password
             username = request.form.get('username')
             password = request.form.get('password')
+            # сравививаем введенные значения с базой данных
             cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
             records = list(cursor.fetchall())
 
@@ -29,13 +34,15 @@ def login():
 
     return render_template('login.html')
 
+# создание декоратора, который регестрирует пользователя
 @app.route('/registration/', methods=['POST', 'GET'])
 def registration():
     if request.method == 'POST':
+        # забираем значения введенные пользователем имя(name), логин(login), пароль(password)
         name = request.form.get('name')
         login = request.form.get('login')
-        password = request.form.get('password')
-
+        password = request.form.get('password')     
+        # добавляем в базу данных введенные пользователем значения
         cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
                        (str(name), str(login), str(password)))
         conn.commit()
